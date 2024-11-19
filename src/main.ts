e4a4ff16d81fe048f93842af87785ab65d07490f
Harshet <d.harshet@nagarro.com>
2024-11-19 10:08:35 +0530

    Toastr Setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createPinia } from "pinia";
 import { createApp } from "vue";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 import App from "./App.vue";
 import router from "./router";
 
 const pinia = createPinia();
 
-createApp(App).use(pinia).use(router).mount("#app");
+createApp(App).use(pinia).use(Toast).use(router).mount("#app");
