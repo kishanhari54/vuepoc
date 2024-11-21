aa6e59d3c4c92b76bd55b0fc0c238fde2093d7ff
Harshet <d.harshet@nagarro.com>
2024-11-21 14:53:24 +0530

    Css changes

--- src/modules/header/components/NavigationComponent.vue
+++ src/modules/header/components/NavigationComponent.vue
@@ -42,36 +42,38 @@ const items = [
 
 <style scoped>
 ul {
+  font-family: Open Sans;
+  font-size: 20px;
+  //font-weight: 600;
+  line-height: 27.24px;
+  text-align: center;
+}
+
+ul {
   list-style-type: none;
   display: flex;
   flex-direction: row;
   gap: 10px;
-  font-family: Open Sans;
-  font-size: 20px;
-  font-weight: 600;
-  line-height: 27.24px;
-
-  text-align: center;
-
   li {
     cursor: pointer;
-    color: #5a5a5a;
     padding: 10px 14px;
-    a {
-      text-decoration: none; /* Remove underline */
-      /* Styling for active links */ /* Use the default Vue Router classes to make active links bold */
-      &.router-link-active {
-        font-weight: bold;
+  }
+}
 
-        color: #000000;
-      }
+a {
+  color: #5a5a5a;
+  text-decoration: none; /* Remove underline */
+  /* Styling for active links */ /* Use the default Vue Router classes to make active links bold */
+  &.router-link-active {
+    font-weight: bold;
 
-      /* Optional: You can also apply styles when it's an exact match */
-      &.router-link-exact-active {
-        font-weight: bold;
-        color: #000000; /* You can also change the color */
-      }
-    }
+    color: #000000;
+  }
+
+  /* Optional: You can also apply styles when it's an exact match */
+  &.router-link-exact-active {
+    font-weight: bold;
+    color: #000000; /* You can also change the color */
   }
 }
 </style>
